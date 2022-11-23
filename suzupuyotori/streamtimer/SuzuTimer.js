2901a19076f066ca2df576dae2cc3bbbb8a8c344
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2022-11-23 18:22:44 +0800

    converted to `hh:mm:ss`

--- suzupuyotori/streamtimer/SuzuTimer.js
+++ suzupuyotori/streamtimer/SuzuTimer.js
@@ -1,6 +1,8 @@
 //timer code is based on W3Schools: https://www.w3schools.com/howto/howto_js_countdown.asp
 //and stackoverflow answer: https://stackoverflow.com/questions/40263537/javascript-countdown-timer-in-gmt-utc
 
+//stackoverflow abt just hh:mm:ss timer: https://stackoverflow.com/questions/55135423/i-need-only-hours-minutes-and-seconds-for-countdown
+
 var countDownDate = new Date("Nov 26, 2022 20:00:00 GMT-05:00").getTime(); //must be 24hr format
 
 var x = setInterval(function () {
@@ -9,11 +11,10 @@ var x = setInterval(function () {
   var distance = countDownDate - now;
 
   var days = ("0" + Math.floor(distance / (1000 * 60 * 60 * 24))).slice(-1);
-  var hours = ("0" + Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).slice(-2);
+  var hours = ("0" + Math.floor(((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + days * 24)).slice(-2);
   var minutes = ("0" + Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))).slice(-2);
   var seconds = ("0" + Math.floor((distance % (1000 * 60)) / 1000)).slice(-2);
 
-  document.getElementById("suzuDays").innerHTML = days + " days to go";
   document.getElementById("suzuHrs").innerHTML = hours;
   document.getElementById("suzuMinsSecs").innerHTML = minutes + " : " + seconds;
 
@@ -21,7 +22,6 @@ var x = setInterval(function () {
 
     clearInterval(x);
 
-    document.getElementById("suzuDays").innerHTML = "00";
     document.getElementById("suzuHrs").innerHTML = "00";
     document.getElementById("suzuMinsSecs").innerHTML = "00 : 00";
     
